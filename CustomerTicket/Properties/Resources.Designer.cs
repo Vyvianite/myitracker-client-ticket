c96b908e516cb2582922b73ff09c952ae1f2a8fd
Daniel <Daniel@PeakTSP.com>
2021-03-10 04:16:43 -0600

    Works as intended. All that's left is connecting to a currently unbuilt endpoint and insuring it can start at boot.

--- CustomerTicket/Properties/Resources.Designer.cs
+++ CustomerTicket/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace CustomerTicket.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace CustomerTicket.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CustomerTicket.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace CustomerTicket.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,12 +61,12 @@ namespace CustomerTicket.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Icon myit {
+        public static System.Drawing.Bitmap MyItIcon {
             get {
-                object obj = ResourceManager.GetObject("myit", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
+                object obj = ResourceManager.GetObject("MyItIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
--- CustomerTicket/TrayContext.cs
+++ CustomerTicket/TrayContext.cs
@@ -19,17 +19,18 @@ namespace CustomerTicket {
       components = new Container();
       notifyIcon = new NotifyIcon( components ) {
         ContextMenuStrip = new(),
-        Icon = Properties.Resources.MyItIcon,
+        Icon = Icon.FromHandle(Properties.Resources.MyItIcon.GetHicon()),
         Text = DefaultTooltip,
         Visible = true
       };
-      notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
+      notifyIcon.ContextMenuStrip.Items.Add( "Exit", null, exitItem_Click );
+      //notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
       notifyIcon.DoubleClick += notifyIcon_DoubleClick;
     }
 
     #region the child forms
     
-    private void ShowForm() {
+    void ShowForm() {
       if ( ticketForm is null ) {
         ticketForm = new TicketForm();
         ticketForm.Closed += ticketFormClosed; // avoid reshowing a disposed form
@@ -40,15 +41,15 @@ namespace CustomerTicket {
       }
     }
 
-    private void notifyIcon_DoubleClick( object sender, EventArgs e ) { ShowForm(); }
+    void notifyIcon_DoubleClick( object sender, EventArgs e ) => ShowForm();
 
-    private void ContextMenuStrip_Opening( object sender, CancelEventArgs e ) {
-      e.Cancel = false;
-      notifyIcon.ContextMenuStrip.Items.Add( "Exit", null, exitItem_Click );
-    }
+    //private void ContextMenuStrip_Opening( object sender, CancelEventArgs e ) {
+    //  e.Cancel = false; /* Add this if the context menu will get populated when opened. Otherwise it will just add extras */
+    //  notifyIcon.ContextMenuStrip.Items.Add( "Exit", null, exitItem_Click );
+    //}
 
     // null out the forms so we know to create a new one.
-    private void ticketFormClosed( object sender, EventArgs e ) { ticketForm = null; }
+    private void ticketFormClosed( object sender, EventArgs e ) => ticketForm = null; 
 
     #endregion the child forms
 
